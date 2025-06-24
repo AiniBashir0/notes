fc6b6856800ab7b19496e990f09c5be871bb00aa
AiniBashir0 <ainyquratulain1270@gmail.com>
2025-06-24 21:37:55 +0500

    webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -49,11 +49,3 @@ resource "aws_instance" "webserver" {
     Name = "webserver"
   }
 }
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "aini-unique-2025-bucket"
-
-  tags = {
-    Name        = "My bucket"
-  }
-}
